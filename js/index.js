6956752a8ef1832681285e3c97081127b3ae26a4
YJKZHUZHU <yejunkuan@163.com>
2019-04-23 19:10:12 +0800

    'pp'

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(function(){
+  load()
   function postaction() {
     if ($('#title').val() == '') {
       alert('不能输入空')
@@ -10,14 +11,22 @@ $(function(){
       data.push(todo)
       // 存储数据
       saveData(data)
-
+      // 重置表单
+      $('#form')[0].reset()
+      load()
     }
   }
+  // 监听回车事件
   $(document).keydown(function (event) {
     if (event.keyCode == 13) {
       postaction()
     }
-  });
+  })
+  $('.deleteToDo').on('click', function () {
+    remove($(this).attr('index'),$(this))
+    // $(this).parent().remove()
+    // window.location.reload()
+  })
   // 数据存localstroge
   function saveData(data) {
     localStorage.setItem('todo', JSON.stringify(data))
@@ -30,5 +39,34 @@ $(function(){
     }else {
       return []
     }
+  }
+  // 填充模板数据
+  function load() {
+    var htmlData = loadData()
+    $('#todo-count').text(htmlData.length)
+    $('#todoList').children().remove()
+    var html = ''
+    for (var i in htmlData) {
+      html += `<li>
+          <input type="checkbox">
+          <p id="todoContent">${htmlData[i].title}</p>
+          <a href="javascript:void(0);" index=${i} class='deleteToDo'>删除</a>
+        </li>`
+    }
+    $('#todoList').append(html)
+  }
+  // 删除待办
+  function remove(index,that){  
+    var removeData = loadData()
+    for (var i in removeData) {
+      if (i == index) {
+        removeData.splice(i,1)
+        that.parent().remove()
+        $('#todo-count').text(removeData.length)
+        
+      }
+      saveData(removeData)
+    }
+   
   }
 })
